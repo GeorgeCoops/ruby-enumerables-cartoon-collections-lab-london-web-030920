7da2aae87bc5e575e54801edfe98788bbb73fb36
George Cooper <coopsgeorge@gmail.com>
2020-03-07 14:40:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array = array.each_with_index { |dwarf, index|
-    puts dwarf
+    puts "#{index}. #{dwarf}"
   }
   array
 end
