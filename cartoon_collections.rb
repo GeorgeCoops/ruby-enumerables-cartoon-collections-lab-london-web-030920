f4fa848f24685f8d1d2e93c28be336555a61aa3c
George Cooper <coopsgeorge@gmail.com>
2020-03-07 14:25:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,6 +3,7 @@ def roll_call_dwarves(array)# code an argument here
 array = array.each do |oppressed_worker|
      puts "#{oppressed_worker.capitalize} wants to start a union!"
   end
+  array
 end
 
 def summon_captain_planet# code an argument here
