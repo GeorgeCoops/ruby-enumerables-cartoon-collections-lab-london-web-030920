c2d4907f9fcda61496e1790f58a37b740de4101d
George Cooper <coopsgeorge@gmail.com>
2020-03-07 15:06:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)# code an argument here
 array.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese9(array)# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   counter = 0
