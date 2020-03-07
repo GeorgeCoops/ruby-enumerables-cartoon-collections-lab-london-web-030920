4c864284277a61b7e7bda615219ad03c1a2807d8
George Cooper <coopsgeorge@gmail.com>
2020-03-07 15:18:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,9 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   counter = 0
 
-  if cheese_types.include?(array[counter])
+  while condition
+    
+  end cheese_types.include?(array[counter])
     return array[counter]
     #binding.pry
   else
