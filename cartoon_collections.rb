0b07c411e055a2d4936b34a532b21b80f90d3163
George Cooper <coopsgeorge@gmail.com>
2020-03-07 14:55:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,10 +15,6 @@ end
 def long_planeteer_calls(array)# code an argument here
   # Your code here
 if array.any? { |word| word.length >= 4 }
-  return true
-else
-  return nil
-end
 end
 
 def find_the_cheese# code an argument here
