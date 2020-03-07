f3446cfc8965584cb36e8e81c42238b919dfc25e
George Cooper <coopsgeorge@gmail.com>
2020-03-07 15:09:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   counter = 0
-  if cheese_types[counter].include?(array[counter])
+  if cheese_types.include?(array[counter])
     return array[counter]
   else
     counter += 1
